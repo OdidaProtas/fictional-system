b71847fe065ed0ca183acb3fef38c50d6d353738
OdidaProtas <bryodiiidah@gmail.com>
2022-01-08 12:07:09 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using C_SHARP_REACT.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace C_SHARP_REACT.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
